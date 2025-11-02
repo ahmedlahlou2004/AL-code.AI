315df31dd7abd6d3dc0bfccb76a7bb3e21db3a01
ahmedlahlou2004 <lahlouahmed1425@gmail.com>
2025-11-02 12:35:17 +0100

    Add Editor and Output components

---
+++ src/components/Editor.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function Editor() {
+  return (
+    <div className="editor-container">
+      <h2>Editor</h2>
+      <textarea placeholder='Write your code here...' />
+    </div>
+  );
+}
+
+export default Editor;
---
+++ src/components/Output.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function Output() {
+  return (
+    <div className="output-container">
+      <h2>Output</h2>
+      <p>Your output will appear here.</p>
+    </div>
+  );
+}
+
+export default Output;
